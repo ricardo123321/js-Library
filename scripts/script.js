df33a3e4d725c83e397110fada56cd782d22c5e2
Ricardo <ricardotvera.54@gmail.com>
2020-09-04 07:55:43 -0700

    added a book, and tinkered with local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-let myLibrary = [];
+if("library" in localStorage) {
+    var myLibrary = localStorage.getItem("library")
+}else{
+    var myLibrary = [];
+    localStorage.setItem("library", myLibrary)
+}
 
 function Book(name, author, pages, read) {
     this.name = name
@@ -7,12 +12,8 @@ function Book(name, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary() {
-    const name = document.getElementById("bookName")
-    const author = document.getElementById("authorName")
-    const pages = document.getElementById("pagesNumber")
-    const rd1 = document.getElementById("yes")
-    const rd2 = document.getElementById("no")
+function addBookToLibrary(name, author, pages, rd1 = false, rd2 = false) {
+    myLibrary = localStorage.getItem("library")
 
     if(rd1.checked==true) {
         var read = rd1}
@@ -20,39 +21,10 @@ function addBookToLibrary() {
         var read = rd2}
     else {
         var read = "unspecified" }
-
-    myLibrary.push(new Book(name, author, pages, read))
-
-    const container = document.getElementById("container")
-    const div = document.createElement("div")
-    const nameB = document.createElement("h1")
-    const authorB = document.createElement("h2")
-    const pagesB = document.createElement("h2")
-    const readB = document.createElement("h3")
-    const btn1  = document.createElement("button")
-    const btn2 = document.createElement("button")
-
-
-    nameB.textContent = myLibrary[myLibrary.length - 1].name
-    authorB.textContent = myLibrary[myLibrary.length - 1].author
-    pagesB.textContent = myLibrary[myLibrary.length - 1].pages
-    readB.textContent = myLibrary[myLibrary.length - 1].read
-    btn1.textContent = "remove"
-
-    btn1.addEventListener("click", () => {
-        container.removeChild(div)
-    })
-
-    div.appendChild(nameB)
-    div.appendChild(authorB)
-    div.appendChild(pagesB)
-    div.appendChild(readB)
-    div.appendChild(btn1)
-    div.appendChild(btn2)
-    container.appendChild(div)
-
     alert(`${name.value} by ${author.value}, ${pages.value} pages, has been added ${read.value}`)
-    console.log(container)
+    console.log(new Book(name, author, pages, read))
+    myLibrary.push(new Book(name, author, pages, read))
+    localStorage.setItem("library", myLibrary)
 }
 
 function forma(){
@@ -62,3 +34,48 @@ function forma(){
 function cancel(){
     document.getElementById("form").style.display = "none"
 }
+
+function cont(){
+    myLibrary = localStorage.getItem("library")
+    const container = document.getElementById("container")
+    var i
+    for (i = 0; i < myLibrary.length; i++){
+        const div = document.createElement("div")
+        const nameB = document.createElement("h1")
+        const authorB = document.createElement("h2")
+        const pagesB = document.createElement("h2")
+        const readB = document.createElement("h3")
+        const btn1  = document.createElement("button")
+        const btn2 = document.createElement("button")
+
+        nameB.textContent = myLibrary[i].name.value
+        authorB.textContent = myLibrary[i].author.value
+        pagesB.textContent = myLibrary[i].pages.value
+        readB.textContent = myLibrary[i].read.value
+        btn1.textContent = "remove"
+        btn2.textContent = "read/unread"
+
+        btn1.addEventListener("click", () => {
+            container.removeChild(div)
+        })
+    
+        btn2.addEventListener("click", () =>{
+            if (readB.value == "Yes I have read it"){
+                readB.textContent = "No I haven't read it" 
+            }else{
+                readB.textContent = "Yes I have read it" 
+            }
+        })
+    
+        div.appendChild(nameB)
+        div.appendChild(authorB)
+        div.appendChild(pagesB)
+        div.appendChild(readB)
+        div.appendChild(btn1)
+        div.appendChild(btn2)
+        container.appendChild(div)
+    }
+}
+
+console.log(localStorage)
+addBookToLibrary("ricardo", "monos", 333, true)
