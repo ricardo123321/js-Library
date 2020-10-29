5e5d39ba33a5c6fd77aaebdddd00e516fc6e48d2
ricardo <ricardotvera.54@gmail.com>
2020-10-28 21:56:07 -0700

    separated the dom from the logic

---
+++ scr/dom.js
@@ -0,0 +1,77 @@
+const dombook1 = () => {
+  name = document.getElementById('bookName').value;
+  author = document.getElementById('authorName').value;
+  pages = document.getElementById('pagesNumber').value;
+  rd1 = document.getElementById('yes');
+  rd2 = document.getElementById('no');
+  let myLibrary = [];
+  let read;
+
+  return{
+    name,
+    author,
+    pages,
+    rd1,
+    rd2,
+    myLibrary,
+    read
+  }
+};
+
+const cont = () => {
+  let myLibrary = JSON.parse(localStorage.getItem('library'));
+  const container = document.getElementById('contn');
+  let i = 0;
+  for (i = 0; i < myLibrary.length; i++) {
+    const div = document.createElement('div');
+    const nameB = document.createElement('h1');
+    const authorB = document.createElement('h2');
+    const pagesB = document.createElement('h2');
+    const readB = document.createElement('h3');
+    const btn1 = document.createElement('button');
+    const btn2 = document.createElement('button');
+    const numB = myLibrary[i].num;
+    nameB.textContent = myLibrary[i].name;
+    authorB.textContent = myLibrary[i].author;
+    pagesB.textContent = myLibrary[i].pages;
+    readB.textContent = myLibrary[i].read;
+    btn1.textContent = 'remove';
+    btn2.textContent = 'read/unread';
+    container.setAttribute('class', 'row');
+    div.setAttribute('class', 'col-lg-4 book');
+    btn1.setAttribute('class', 'btn btn-primary');
+    btn2.setAttribute('class', 'btn btn-primary');
+
+    btn1.addEventListener('click', () => {
+      const result = myLibrary.find(({ num }) => num === numB);
+      if (myLibrary.length === 1) {
+        myLibrary = [];
+      }
+      myLibrary.splice(numB, 1);
+      i -= 1;
+      container.removeChild(div);
+      localStorage.setItem('library', JSON.stringify(myLibrary));
+      localStorage.setItem('counter', JSON.stringify(i));
+    });
+
+    btn2.addEventListener('click', () => {
+      if (readB.textContent === 'Yes I have read it') {
+        readB.textContent = "No I haven't read it";
+        myLibrary[numB].read = "No I haven't read it";
+        localStorage.setItem('library', JSON.stringify(myLibrary));
+      } else {
+        readB.textContent = 'Yes I have read it';
+        myLibrary[numB].read = 'Yes I have read it';
+        localStorage.setItem('library', JSON.stringify(myLibrary));
+      }
+    });
+
+    div.appendChild(nameB);
+    div.appendChild(authorB);
+    div.appendChild(pagesB);
+    div.appendChild(readB);
+    div.appendChild(btn1);
+    div.appendChild(btn2);
+    container.appendChild(div);
+  }
+};
--- scr/script.js
+++ scr/script.js
@@ -15,31 +15,25 @@ const Book = (name, author, pages, read, num) => ({
 );
 
 const addBookToLibrary = (name, author, pages, rd1 = false, rd2 = false) => {
-  name = document.getElementById('bookName').value;
-  author = document.getElementById('authorName').value;
-  pages = document.getElementById('pagesNumber').value;
-  rd1 = document.getElementById('yes');
-  rd2 = document.getElementById('no');
-  let myLibrary = [];
-  let read;
+  let bb = dombook1()
   if ('library' in localStorage && localStorage.getItem('library') !== '') {
-    myLibrary = JSON.parse(localStorage.getItem('library'));
+    bb.myLibrary = JSON.parse(localStorage.getItem('library'));
   }
 
   let i = JSON.parse(localStorage.getItem('counter'));
   const num = i;
 
-  if (rd1.checked === true) {
+  if (bb.rd1.checked === true) {
     read = rd1.value;
-  } else if (rd2.checked === true) {
-    read = rd2.value;
+  } else if (bb.rd2.checked === true) {
+    read = bb.rd2.value;
   } else {
     read = 'unspecified';
   }
 
-  myLibrary[i] = Book(name, author, pages, read, num);
+  bb.myLibrary[i] = Book(bb.name, bb.author, bb.pages, bb.read, num);
   i += 1;
-  localStorage.setItem('library', JSON.stringify(myLibrary));
+  localStorage.setItem('library', JSON.stringify(bb.myLibrary));
   localStorage.setItem('counter', JSON.stringify(i));
 };
 
@@ -49,63 +43,5 @@ const forma = () => {
 
 const cancel = () => {
   document.getElementById('form').style.display = 'none';
-};
-
-const cont = () => {
-  let myLibrary = JSON.parse(localStorage.getItem('library'));
-  const container = document.getElementById('contn');
-  let i = 0;
-  for (i = 0; i < myLibrary.length; i++) {
-    const div = document.createElement('div');
-    const nameB = document.createElement('h1');
-    const authorB = document.createElement('h2');
-    const pagesB = document.createElement('h2');
-    const readB = document.createElement('h3');
-    const btn1 = document.createElement('button');
-    const btn2 = document.createElement('button');
-    const numB = myLibrary[i].num;
-    nameB.textContent = myLibrary[i].name;
-    authorB.textContent = myLibrary[i].author;
-    pagesB.textContent = myLibrary[i].pages;
-    readB.textContent = myLibrary[i].read;
-    btn1.textContent = 'remove';
-    btn2.textContent = 'read/unread';
-    container.setAttribute('class', 'row');
-    div.setAttribute('class', 'col-lg-4 book');
-    btn1.setAttribute('class', 'btn btn-primary');
-    btn2.setAttribute('class', 'btn btn-primary');
-
-    btn1.addEventListener('click', () => {
-      const result = myLibrary.find(({ num }) => num === numB);
-      if (myLibrary.length === 1) {
-        myLibrary = [];
-      }
-      myLibrary.splice(numB, 1);
-      i -= 1;
-      container.removeChild(div);
-      localStorage.setItem('library', JSON.stringify(myLibrary));
-      localStorage.setItem('counter', JSON.stringify(i));
-    });
-
-    btn2.addEventListener('click', () => {
-      if (readB.textContent === 'Yes I have read it') {
-        readB.textContent = "No I haven't read it";
-        myLibrary[numB].read = "No I haven't read it";
-        localStorage.setItem('library', JSON.stringify(myLibrary));
-      } else {
-        readB.textContent = 'Yes I have read it';
-        myLibrary[numB].read = 'Yes I have read it';
-        localStorage.setItem('library', JSON.stringify(myLibrary));
-      }
-    });
-
-    div.appendChild(nameB);
-    div.appendChild(authorB);
-    div.appendChild(pagesB);
-    div.appendChild(readB);
-    div.appendChild(btn1);
-    div.appendChild(btn2);
-    container.appendChild(div);
-  }
 };
 cont();
